b3cd911ad631a861d470051e05d3ebab0969688a
Dev Jaikalyani <81683639+devjaikalyani@users.noreply.github.com>
2024-12-23 19:56:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+}); 
+
+app.get("/about", (req,res) => {
+  res.render("about.ejs");
+})
+
+app.get("/faqs", (req,res) => {
+  res.render("faqs.ejs");
+})
+
+
+app.set('view engine', 'ejs');
+
+let blogs = [];
+
+app.get("/create", (req,res) => {
+  res.render("create.ejs");
+})
+
+app.post('/create', async (req, res) => {
+    const { title, content, author } = req.body;
+ 
+    const newBlogPost = {
+      id: blogs.length + 1,
+      title,
+      content,
+      publishDate: new Date(),
+      author,
+      visibility: 'public'
+    };
+
+    blogs.push(newBlogPost);
+    res.redirect('/explore');
+});
+
+app.get('/explore', (req, res) => {
+    const publicBlogs = blogs.filter(blog => blog.visibility === 'public');
+
+  res.render('explore', { blogs: publicBlogs });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const blogId = parseInt(req.params.id);
+  const blog = blogs.find(blog => blog.id === blogId);
+
+  if (!blog) {
+    return res.status(404).send('Blog not found');
+  }
+
+  res.render('blog-detail', { blog });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
